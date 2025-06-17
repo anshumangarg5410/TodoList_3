e5bbe6ffc3a709e409d8568c91b4f228c7c99917
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-17 17:43:12 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/TodoList_3.git/",
+  base: "/TodoList_3/",
   plugins: [react()],
 })
