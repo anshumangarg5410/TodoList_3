b24b7e196198d744a957a7eb6273d75e3e106a01
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-18 09:56:46 +0530

    redux ne kra dimag kharab

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,3 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
+import {todoReducer} from '../fearures/todo/todoslice'
 
-export const store = configureStore({});
+export const store = configureStore({
+    reducer: todoReducer
+});
+
--- src/fearures/todo/todoslice.js
+++ src/fearures/todo/todoslice.js
@@ -24,4 +24,9 @@ export const todoSlice = createSlice({
 
 
     }
+
 }) 
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
