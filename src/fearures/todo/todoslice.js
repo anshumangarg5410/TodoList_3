441364b73eb722eff8d2719aa9baf636882af82d
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-18 12:42:30 +0530

    functions made

--- src/fearures/todo/todoslice.js
+++ src/fearures/todo/todoslice.js
@@ -3,8 +3,14 @@ import {createSlice, nanoid} from '@redusjs./toolkit';
 const initialState = {
     todos: [{
         id: 1,
-        text: "Hello World"
-    }]
+        text: "Hello World",
+        completed: false
+    }],
+    // completedtodo: [{
+    //     id: 1,
+    //     text: "Hello World",
+    //     completed: true
+    // }]
 }
 
 export const todoSlice = createSlice({
@@ -22,7 +28,10 @@ export const todoSlice = createSlice({
             state.todos = state.todos.filter((todo) => todo.id != action.payload)
         },
         editTodo: (state, action) => {
-            
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id ? {...todo, text: action.payload.text} : todo)
+        },
+        toggleTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id ? {...todo, completed: !todo.completed} : todo)
         }
 
     }
