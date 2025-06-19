d715d6f17cc025ab90888ffb7e8c714de6205fe3
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-19 15:30:12 +0530

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Item from "./components/Item";
 
 function App() {
 
-
-
-  // const todos = useSelector(state => state.todos)
-  // const completed = useSelector(state => state.completed)
-
   const [completedarea, setcompletedarea] = useState(true);
 
 
@@ -27,7 +22,7 @@ function App() {
 
   return (
     <>
-      <div className="main bg-[url(./assets/bg7.webp)] bg-cover bg-no-repeat absolute inset-0 bg-black/40 z-0 bg-center h-screen w-screen flex flex-col justify-start items-center overflow-hidden">
+      <div className="main bg-[url(./assets/bg7.webp)] bg-cover bg-no-repeat bg-center h-screen w-screen flex flex-col justify-start items-center overflow-hidden">
         <div className="px-4 backdrop-blur-md bg-[#0d0d0d]/40 h-[10%] w-[98%] rounded-lg  mt-4 flex justify-between items-center shadow-[0_0_3px_#00ffff]">
           <h1 className="text-3xl font-bold text-[#00ffff] drop-shadow-[0_0_8px_#00ffff] tracking-wide font-mono">
             ⚡ Todo List
@@ -55,7 +50,7 @@ function App() {
           </div>
 
 
-           <div className={`box2 rounded-lg shadow-[0_0_3px_#00ffff] h-[95%] w-[49%] backdrop-blur-md bg-[#0d0d0d]/40  ${completedarea ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-90 pointer-events-none w-[0%]"} transition-all ease-in-out duration-[500]`}>
+           <div className={`box2 rounded-lg shadow-[0_0_3px_#00ffff] h-[95%] w-[49%] backdrop-blur-md bg-[#0d0d0d]/40  ${completedarea ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-90 pointer-events-none hidden w-[0%]"} transition-all ease-in-out duration-[500]`}>
             <div className="heading h-[10%] w-[49%] flex justify-start px-4 items-center text-[17px] font-bold font-sans ">
               <h1 className="text-[#00ffff] w-[250px] drop-shadow-[0_0_1px_#00ffff] border-b border-[#00ffff] border-double p-2">
                 Completed Tasks
